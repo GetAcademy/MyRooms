3be3fb491030f1670c2387eeed75082c482488b6
GET Academy <terje@getacademy.no>
2022-09-05 10:39:19 +0200

    Initial commit

---
+++ MyRooms/House.cs
@@ -0,0 +1,40 @@
+﻿namespace MyRooms
+{
+    internal class House
+    {
+        public char Room { get; private set; }
+
+        public House()
+        {
+            Room = 'A';
+        }
+
+        public void Move(char newRoom)
+        {
+            if (Room == 'A' && newRoom is 'B' or 'D')
+            {
+                Room = newRoom;
+            }
+            else if (Room == 'B' && newRoom is 'A' or 'C' or 'E')
+            {
+                Room = newRoom;
+            }
+            else if (Room == 'C' && newRoom is 'B')
+            {
+                Room = newRoom;
+            }
+            else if (Room == 'D' && newRoom is 'A')
+            {
+                Room = newRoom;
+            }
+            else if (Room == 'E' && newRoom is 'B' or 'F')
+            {
+                Room = newRoom;
+            }
+            else if (Room == 'F' && newRoom is 'E')
+            {
+                Room = newRoom;
+            }
+        }
+    }
+}
---
+++ MyRooms/Program.cs
@@ -0,0 +1,52 @@
+﻿using MyRooms;
+
+// pause til 
+
+var house = new House();
+
+while (true)
+{
+    Console.WriteLine($"Du er i rom {house.Room}");
+    Console.Write("Hvor vil du gå? ");
+    var roomStr = Console.ReadLine();
+    var newRoom = roomStr[0];
+    house.Move(newRoom);
+}
+
+/*
+var room = 'A';
+
+while (true)
+{
+    Console.WriteLine($"Du er i rom {room}");
+    Console.Write("Hvor vil du gå? ");
+    var roomStr = Console.ReadLine();
+    var newRoom = roomStr[0];
+    if (room == 'A' && newRoom is 'B' or 'D')
+    {
+        room = newRoom;
+    }
+    else if (room == 'B' && newRoom is 'A' or 'C' or 'E')
+    {
+        room = newRoom;
+    }
+    else if (room == 'C' && newRoom is 'B')
+    {
+        room = newRoom;
+    }
+    else if (room == 'D' && newRoom is 'A')
+    {
+        room = newRoom;
+    }
+    else if (room == 'E' && newRoom is 'B' or 'F')
+    {
+        room = newRoom;
+    }
+    else if (room == 'F' && newRoom is 'E')
+    {
+        room = newRoom;
+    }
+}
+
+ 
+ */
